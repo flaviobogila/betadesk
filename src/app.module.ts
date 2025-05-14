c612a2d1c002fe13b1d1fd28ec4ebda4f7cafe69
Flávio Bogila <flavio.bogila@bluecore.it>
2025-05-13 22:06:55 -0300

    pagina home estatica

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,10 +14,17 @@ import { LabelsModule } from './labels/labels.module';
 import { ConversationLabelsModule } from './conversation-labels/conversation-labels.module';
 import { TeamsModule } from './teams/teams.module';
 import { TeamMembersModule } from './team-members/team-members.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [PrismaModule, UsersModule, TenantsModule, ChannelsModule, MessagesModule, ContactsModule, ConversationsModule, WebhookModule, AuthModule, LabelsModule, ConversationLabelsModule, TeamsModule, TeamMembersModule],
-  controllers: [AppController],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'), // caminho para a pasta 'public'
+      exclude: ['/api*'], // exclui rotas da API, se houver
+    }),
+    PrismaModule, UsersModule, TenantsModule, ChannelsModule, MessagesModule, ContactsModule, ConversationsModule, WebhookModule, AuthModule, LabelsModule, ConversationLabelsModule, TeamsModule, TeamMembersModule],
+  //controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
